7e71d27bd8852e64e75d4f943f018472be38caeb
Student User <student@workstation.lab.example.com>
2022-03-03 06:01:43 -0500

    added variable, forgot semicolon

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
-$name = "Morten"
+$name = "Morten";
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
-	<div>Hello <?php $name?>, this is from the OpenShift Console</div>
+	<div>Hello <?php $name ?>, this is from the OpenShift Console</div>
